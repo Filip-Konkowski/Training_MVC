3436e1e0dacf3f61546ccddb32e6779973fe9a81
Filip-Konkowski <fkonkowski@gmail.com>
2015-10-10 15:33:47 +0200

    upto file .htaccess

---
+++ app/core/App.php
@@ -0,0 +1,16 @@
+<?php
+ class App {
+    protected $controller = "home";
+    protected $method = "index";
+    protected $parameters = [];
+
+    public function __construct(){
+        $this->parseUrl();
+    }
+
+    public function parseUrl(){
+        if(isset($_GET['url'])){
+            echo $_GET['url'];
+        }
+    }
+ }
